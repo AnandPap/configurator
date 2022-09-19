2af157b04cf77732013cd9f66bcdd780c0f07b90
Anand Pap <anandpap@live.com>
2022-09-19 08:10:19 +0200

    handling form input

--- src/reusable/FormField.js
+++ src/reusable/FormField.js
@@ -1,16 +1,34 @@
 import React, { useState } from "react";
 
-const FormField = ({ formFieldTitle, type }) => {
+const FormField = ({
+  formFieldTitle,
+  type,
+  setFormData,
+  warningMessage,
+  setWarningMessage,
+}) => {
   const [inputText, setInputText] = useState("");
+
+  const inputHandler = (e) => {
+    if (warningMessage !== "") setWarningMessage("");
+    setInputText(e.target.value);
+    if (type === "text")
+      setFormData((s) => ({ ...s, address: e.target.value }));
+    else if (type === "tel")
+      setFormData((s) => ({ ...s, telefon: e.target.value }));
+    else setFormData((s) => ({ ...s, [type]: e.target.value }));
+  };
+
   return (
     <div className="form-field">
-      <div className="form-field-title">{formFieldTitle}</div>
+      <div className="form-field-title">{formFieldTitle} *</div>
       <input
         className="form-input"
         type={type}
         placeholder="Antwort hier"
         value={inputText}
-        onChange={(e) => setInputText(e.target.value)}
+        onChange={inputHandler}
+        required
       />
     </div>
   );
