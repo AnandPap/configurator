2d4f51c3a14957eb54d52d1e43ebf0169980b4e9
Anand Pap <anandpap@live.com>
2022-09-19 07:24:00 +0200

    made small adjustments

--- src/ThankYouSlide.js
+++ src/ThankYouSlide.js
@@ -2,11 +2,17 @@ import React, { useState, useEffect } from "react";
 
 const ThankYouSlide = ({ slideNumber }) => {
   const [thankYouSlideClassName, setThankYouSlideClassName] = useState("hide");
+
   useEffect(() => {
     if (slideNumber.current === 6)
       setThankYouSlideClassName("fade-in-from-right");
   }, [slideNumber]);
-  return <div className={`${thankYouSlideClassName}`}>ThankYouSlide</div>;
+
+  return (
+    <div className={`thank-you-screen ${thankYouSlideClassName}`}>
+      Vielen Dank!
+    </div>
+  );
 };
 
 export default ThankYouSlide;
